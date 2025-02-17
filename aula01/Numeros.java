a2cc3d59a0f34f0a5848d205fc3dee50587b7d31
yasmim-nappi <martins.k.nappi@gmail.com>
2025-02-17 09:20:03 -0300

    Aula sobre estrutura e códigos básicos em Java.

---
+++ aula01/Numeros.java
@@ -0,0 +1,9 @@
+public class Numeros {
+                  public static void main (String args[]) {
+               System.out.println(34.5 *2);
+               System.out.println(57 + 12);
+               System.out.println(-231 + 300);
+               System.out.println(23*3);
+               System.out.println(1200-1131);
+          }
+     }
