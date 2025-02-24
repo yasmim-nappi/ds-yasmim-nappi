a180dfa43af7a741d7fcf46f0a74c24da791898a
yasmim-nappi <martins.k.nappi@gmail.com>
2025-02-24 09:33:05 -0300

    Introdução a variáveis em Java.

---
+++ aula 02/variaveis.java
@@ -0,0 +1,14 @@
+public class variaveis {
+    public static void main(String[] args) {
+      String nome = "Darwin Raglan Caspian Ahab Poseidon Nicodemius Watterson III";
+      int num = 69;
+      float dec = 101.24f;
+      boolean consegui = true;
+      char ass = 'Y';
+      System.out.println(num);
+      System.out.println(nome);
+      System.out.println(dec);
+      System.out.println(dec + num);
+      System.out.println(num+"\n"+nome+"\n"+dec+"\n"+consegui+"\n"+"\n"+ass);
+    }
+  }
