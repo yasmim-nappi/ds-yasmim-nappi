f1060f57daa5558c6f7e1f105bde2f370a86bc82
Yasmim <martins.k.nappi@gmail.com>
2025-03-17 09:32:23 -0300

    Condições em Java e uso da classe Scanner.

--- aula04/Condicao.java
+++ aula04/Condicao.java
@@ -1,18 +1,22 @@
-import java.until.Scanner;
+import java.util.Scanner;
 public class Condicao {
     public static void main (String[] args) {
         Scanner v1= new Scanner (System.in);
-        System.out.println("Entre um número: ");
+        System.out.println("Entre um numero: ");
         int n1= v1.nextInt();
         Scanner v2= new Scanner (System.in);
-        System.out.println("Digite outro número: ");
+        System.out.println("Digite outro numero: ");
         int n2 = v2.nextInt();
         v1.close();
         v2.close();
         if (n1 > n2) {
             System.out.printIn(n1 + " é maior que " + n2);
+        } 
+        else if (n1 == n2){
+            System.out.printIn(n1 + " é igual a " + n2);
         } else {
-            System.out.printIn(n1 + " é menor que " + n2);
+            System.out.printIn (n1 + "é menor que " + n2);
+        }
         }
     }
 }
