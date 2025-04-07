4bcb41ca8d357b21812e6b00ba2af2484dc0dbee
Yasmim <martins.k.nappi@gmail.com>
2025-04-07 09:29:36 -0300

    Exercícios de Java.

---
+++ exercícios/Exercício02.java
@@ -0,0 +1,17 @@
+import java util.Scanner;
+
+public class Exercício02 {
+ public static void main (String[] args) {
+   Scanner input = new Scanner(System.in);
+
+   int id;
+   char genero;
+   int anos;
+   float salario;
+   float bonus;
+
+   System.out.printIn("\nBônus de Natal!!\n 
+   Você, nosso funcionário, merece um prestígio para aproveitar o fim de ano sem miséria!
+   \nDigite seu ID: ")
+  }
+}
