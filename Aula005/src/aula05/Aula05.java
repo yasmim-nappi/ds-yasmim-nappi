b3d6efd7f9289594e035bc9350e7a9612fc894b6
Yasmim <martins.k.nappi@gmail.com>
2025-04-28 09:31:30 -0300

    aula 05

---
+++ Aula005/src/aula05/Aula05.java
@@ -0,0 +1,7 @@
+package aula05;
+
+public class Aula05 {
+    public static void main(String[] args) {
+        System.out.println("uff! fire fire");
+    } 
+}
---
+++ Aula005/src/aula05/Exercicio01.java
@@ -0,0 +1,11 @@
+package aula05;
+
+public class Exercicio01{
+        public static void main(String[] args) {
+            int GastosJaneiro = 15000;
+            int GastosFevereiro = 23000;
+            int GastosMarco = 17000;
+            int gastosTrimestre =   GastosJaneiro + GastosFevereiro + GastosMarco;
+            System.out.println(gastosTrimestre);
+        }  
+    }
