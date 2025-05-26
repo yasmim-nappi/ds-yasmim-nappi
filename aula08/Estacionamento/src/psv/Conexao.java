89c7f941dac670464f3bfd0740f8421052cebed4
Yasmim <martins.k.nappi@gmail.com>
2025-05-26 09:29:29 -0300

    Aplicação simples de consulta em um banco de dados, utilizando Java, Netbeans e MysSQL.

---
+++ aula08/Estacionamento/src/psv/Conexao.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package psv;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Conexao {
+    
+}
