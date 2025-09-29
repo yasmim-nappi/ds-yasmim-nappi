fe7f960e0f68bd5b2874f216e7b0804cbce4cd76
Yasmim <martins.k.nappi@gmail.com>
2025-09-29 09:19:43 -0300

    Primeiras configurações da API REST simples.

---
+++ bancodados/src/main/java/banco/bancodados/BancodadosApplication.java
@@ -0,0 +1,13 @@
+package banco.bancodados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancodadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancodadosApplication.class, args);
+	}
+
+}
---
+++ bancodados/src/test/java/banco/bancodados/BancodadosApplicationTests.java
@@ -0,0 +1,13 @@
+package banco.bancodados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancodadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
